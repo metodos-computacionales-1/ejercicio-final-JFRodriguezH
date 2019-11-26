09ba8517322b2757ad6d6c677a470688b273dabd
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-26 14:05:44 +0000

    Codigo en python para graficar el diagrama de bifurcacion

---
+++ pendulo_dos.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+f = np.array([1.35, 1.36, 1.37, 1.38, 1.39, 1.4, 1.41, 1.42, 1.43, 1.44, 1.45, 1.46, 1.47, 1.48, 1.49, 1.5])
+plt.figure()
+for i in f:
+	data = np.loadtxt("pendulo_"+str(i)+".dat")
+	t = data[:, 0]
+	F = t[0]
+	theta = data[:, 1]
+	Omega = theta[0]
+	T = 2*np.pi/Omega
+	w = data[:, 2]
+	dt = w[0]
+	N = int(len(t)/2)
+	t = t[N:]
+	q = (t%T<0.01)
+	theta = theta[N:]
+	k = np.ones(len(theta[q]))*i
+	plt.scatter(k, theta[q], color='k')
+plt.savefig("bifurcacion.png")
+
+
