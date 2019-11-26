ef5d575f6440d2c64d54bf96904ae52d87d5d24a
JFRodriguezH <jf.rodriguezh@uniandes.edu.co>
2019-11-26 12:26:22 +0000

    Makefile y python que generan las figuras solicitadas en el punto 1

---
+++ makefile
@@ -0,0 +1,8 @@
+uno : 
+	c++ pendulo.cpp -o pendulo.out
+	./pendulo.out 100000 1.4
+	python pendulo.py
+	./pendulo.out 100000 1.44
+	python pendulo.py
+	./pendulo.out 100000 1.465
+	python pendulo.py
--- pendulo.py
+++ pendulo.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 data = np.loadtxt("pendulo.dat")
 
 t = data[:, 0]
+F = t[0]
 N = len(t)
 theta = data[:, 1]
 w = data[:, 2]
 
 plt.figure()
-plt.plot(t[int(N/2):], theta[int(N/2):])
-plt.savefig("pendulo.png")
+plt.plot(w[int(N/2):], theta[int(N/2):])
+plt.savefig("poincare_"+str(F)+".png")
